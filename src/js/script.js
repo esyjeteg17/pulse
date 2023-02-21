36bc59b615ec27f25fffd0c95f678a596bcb36ae
Kirill <esyeteg17@gmail.com>
2023-02-21 22:56:59 +0300

    carousel

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick(
+        {
+            speed: 1200,
+            prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png"></button>',
+        }
+    );
+  });
